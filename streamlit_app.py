65154856941c0a86b7b505bb3a60d5ca17ad97d3
JulianHristov <55739305+JulianHristov@users.noreply.github.com>
2022-12-06 16:43:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,19 @@ streamlit.dataframe(my_fruit_list)
 
 
 
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please insert fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+except URLError as e:
+  streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
