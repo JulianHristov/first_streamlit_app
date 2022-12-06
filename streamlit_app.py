5ee8db97d4076ccfcb78ec85304145e2e315e6dc
JulianHristov <55739305+JulianHristov@users.noreply.github.com>
2022-12-06 16:55:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 def get_fruit_load_list():
-    with my_cur = my_cnx.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
         return my_cur.fetchall()
 
